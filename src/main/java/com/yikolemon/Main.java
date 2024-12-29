04a21f18e1f4cfc94a7d8f2d4b372c2b0893717b
yikolemon <1142496307@qq.com>
2024-12-29 19:57:47 +0800

    :tada: first version

---
+++ src/main/java/com/yikolemon/Main.java
@@ -0,0 +1,31 @@
+package com.yikolemon;
+
+import cn.hutool.cron.CronUtil;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        // 设置 Cron 表达式，表示每天凌晨 4 点执行任务
+        // 秒 分 时 日 月 周年
+        String cronExpression = "0 0 4 * * ?";
+        // 设置任务执行逻辑
+        CronUtil.schedule(cronExpression, new PortRunnable());
+        // 支持秒级别的定时任务
+        CronUtil.setMatchSecond(true);
+        // 启动定时任务调度
+        CronUtil.start();
+        sleep();
+    }
+
+    private static void sleep(){
+        // 让主线程保持活动，直到程序需要退出
+        // 这里使用 Thread.sleep(Long.MAX_VALUE) 来阻塞主线程
+        try {
+            // 主线程进入无限阻塞状态，直到程序被外部停止
+            Thread.sleep(Long.MAX_VALUE);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/yikolemon/PortRunnable.java
@@ -0,0 +1,168 @@
+package com.yikolemon;
+
+import cn.hutool.http.ContentType;
+import cn.hutool.http.HttpRequest;
+import cn.hutool.http.HttpResponse;
+import cn.hutool.http.HttpUtil;
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONArray;
+import com.alibaba.fastjson2.JSONObject;
+import com.alibaba.fastjson2.util.PropertiesUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+/**
+ * @author yikolemon
+ */
+public class PortRunnable implements Runnable{
+
+    private Integer count = 0;
+
+    private static final Properties P = new Properties();
+
+    static {
+        // 获取当前类所在的 JAR 文件路径
+        Properties properties = loadOutterProperties();
+        P.putAll(properties);
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        InputStream resource = classLoader.getResourceAsStream("application.properties");
+        try {
+            P.load(resource);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void run() {
+        try{
+            login();
+            Map<String, Object> first = getFirst();
+            updatePort(first);
+        }
+        catch (Exception e){
+            if (count <= 3){
+                count++;
+                this.run();
+            }else {
+                throw e;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new PortRunnable().run();
+    }
+
+    private void login(){
+        String username = P.getProperty("name");
+        String password = P.getProperty("password");
+        String url = P.getProperty("url");
+        HttpRequest post = HttpUtil.createPost(url + "login");
+        // 设置 Content-Type 为 form-data
+        post.contentType(ContentType.FORM_URLENCODED.getValue());
+        Map<String, String> data = new HashMap<>();
+        data.put("username", username);
+        data.put("password", password);
+        post.body(HttpUtil.toParams(data)).execute();
+    }
+
+//    private HttpCookie getCookie(HttpResponse resp){
+//        List<HttpCookie> cookies = resp.getCookies();
+//        return cookies.get(0);
+//    }
+
+    private Map<String, Object> getFirst(){
+        String url = P.getProperty("url");
+        HttpRequest post = HttpUtil.createPost(url + "xui/inbound/list");
+        HttpResponse execute = post.execute();
+        String body = execute.body();
+        JSONObject jsonObject = JSONObject.parseObject(body);
+        // 获取 "obj" 数组
+        JSONArray objArray = jsonObject.getJSONArray("obj");
+
+        // 处理 obj 数组中的每个元素
+        for (int i = 0; i < objArray.size(); i++) {
+            JSONObject objItem = objArray.getJSONObject(i);
+
+            // 获取并解析嵌套的 JSON 字符串
+            String settingsStr = objItem.getString("settings");
+            String streamSettingsStr = objItem.getString("streamSettings");
+            String sniffingStr = objItem.getString("sniffing");
+
+            // 将嵌套 JSON 字符串转换为 JSONObject
+            JSONObject settingsJson = JSON.parseObject(settingsStr);
+            JSONObject streamSettingsJson = JSON.parseObject(streamSettingsStr);
+            JSONObject sniffingJson = JSON.parseObject(sniffingStr);
+
+            // 将解析后的 JSON 放回原来的 objItem 中
+            objItem.put("settings", settingsJson);
+            objItem.put("streamSettings", streamSettingsJson);
+            objItem.put("sniffing", sniffingJson);
+        }
+        Object obj = jsonObject.getJSONArray("obj").get(0);
+        if (obj instanceof JSONObject){
+            JSONObject resObj = (JSONObject) obj;
+            return resObj.toJavaObject(Map.class);
+        }
+        return null;
+    }
+
+    private void updatePort(Map<String, Object> map){
+        String url = P.getProperty("url");
+        HttpRequest post = HttpUtil.createPost(url + "xui/inbound/update/1");
+        // 设置 Content-Type 为 form-data
+        post.contentType(ContentType.FORM_URLENCODED.getValue());
+        String port = getPort();
+        map.put("port", port);
+        HttpResponse execute = post.body(HttpUtil.toParams(map)).execute();
+        System.out.println(execute);
+    }
+
+    private static String getPort() {
+        // 获取当前时间
+        LocalDateTime now = LocalDateTime.now();
+        // 获取年份的最后一位
+        int yearLastDigit = now.getYear() % 10;
+        // 保持两位格式
+        String month = String.format("%02d", now.getMonthValue());
+        // 保持两位格式
+        String day = String.format("%02d", now.getDayOfMonth());
+        // 拼接结果
+        return String.format("%d%s%s", yearLastDigit, month, day);
+    }
+
+    /**
+     * 加载同级的配置文件
+     * @return 配置类
+     */
+    private static Properties loadOutterProperties(){
+        // 获取当前 JAR 文件的路径
+        String jarPath = Main.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+        File jarDir = new File(jarPath).getParentFile();
+
+        if (jarDir != null) {
+            // 构造 application.properties 的文件路径
+            File propertiesFile = new File(jarDir, "application.properties");
+
+            if (propertiesFile.exists()) {
+                System.out.println("配置文件路径: " + propertiesFile.getAbsolutePath());
+
+                // 使用 Hutool 的 PropertiesUtil 读取配置文件
+                return PropertiesUtils.toProperties(propertiesFile);
+            } else {
+                System.out.println("配置文件不存在");
+            }
+        } else {
+            System.out.println("无法获取 JAR 所在的目录");
+        }
+        return null;
+    }
+
+}
